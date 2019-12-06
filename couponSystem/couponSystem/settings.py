977861fd913cefd4640840e51f47b5cd67b01eb8
DhruvKaushal <dhruvkaushal1997@gmail.com>
2019-12-07 05:26:13 +0530

    logins

--- couponSystem/couponSystem/settings.py
+++ couponSystem/couponSystem/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
+    'bootstrap4',
+    'accounts.apps.AccountsConfig',
+    'profiles'
 ]
 
 MIDDLEWARE = [
