016a4937304b6f950d00a221955622ba055b8135
DhruvKaushal <dhruvkaushal1997@gmail.com>
2019-12-09 21:58:50 +0530

    user info

--- couponSystem/profiles/models.py
+++ couponSystem/profiles/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import datetime
 # Create your models here.
  
+class userData(models.Model):
+    name = models.CharField(max_length=30, on_delete=models.CASCADE)
+    balance = models.IntegerField()
+    date_of_payment = models.DateTimeField(default = datetime.now())
+    vendor_number  = models.
