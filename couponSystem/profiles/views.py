c99d8e9b2937992f562169277c8ecbed1f24fdde
DhruvKaushal <dhruvkaushal1997@gmail.com>
2019-12-10 07:16:33 +0530

    admin info home

--- couponSystem/profiles/views.py
+++ couponSystem/profiles/views.py
@@ -3,12 +3,12 @@ from django.contrib.auth.mixins import(
     LoginRequiredMixin,
     PermissionRequiredMixin
 )
+# from django.forms.models import model_to_dict
 from .models import userData
 # Create your views here.
 def adminKaPage(request):
-    model = userData
-    records = model.objects.all().order_by(model.name)
-    return render(request, 'profiles/adminKaLogin.html')
+    records = userData.objects.all()
+    return render(request, 'profiles/adminKaLogin.html',{'model':records})
 
 
 def userKaLog(request):
