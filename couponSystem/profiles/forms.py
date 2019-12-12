56520d519291ea01fcefc08df4d78f1251e379f7
DhruvKaushal <dhruvkaushal1997@gmail.com>
2019-12-12 23:45:57 +0530

    started with user home page

--- couponSystem/profiles/forms.py
+++ couponSystem/profiles/forms.py
@@ -2,9 +2,10 @@ from django import forms
 from django.contrib.auth import get_user_model
 
 vendor_choices = [
-    ('Vendor 1'), ('Vendor 2'), ('Vendor 3'),
+    ('1','Vendor 1'),('2',"Vendor 2"), ("3", "Vendor 3"),
 ]    
 
 class select_vendor_form(forms.Form):
-    vendor = forms.CharField(label='Select the desired vendor', widget = forms.RadioSelect(choices=vendor_choices))
+    Vendor_choices = forms.ChoiceField(choices=vendor_choices,
+                                widget=forms.RadioSelect)
     
--- couponSystem/profiles/views.py
+++ couponSystem/profiles/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.mixins import(
     LoginRequiredMixin,
     PermissionRequiredMixin
 )
+from .forms import select_vendor_form
+from django.db.models import Sum
+
 # from django.forms.models import model_to_dict
 from .models import userData
 from . import forms
@@ -12,5 +15,17 @@ def adminKaPage(request):
     return render(request, 'profiles/adminKaLogin.html',{'model':records})
 
 
+# def userKaLog(request):
+#     form  = select_vendor_form()
+#     return render(request, 'profiles/userLogin.html', {'form': form})
+
 def userKaLog(request):
-    return render(request, 'profiles/userLogin.html')
+    form = select_vendor_form()
+    if request.method=="POST" and form.is_valid():
+        choice = form.cleaned_data['Vendor_choices']
+        print(choice)
+        records = userData.objects.filter(Vendor_number = ('choice')).aggregate(Sum('itemPrice'))
+        args = {'form': form, 'choice': choice, 'model':records}
+        return render(request, 'profiles/useLogin.html', args)
+    else:
+        return render(request, 'profiles/userLogin.html', {'form': form})
