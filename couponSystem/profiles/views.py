77a367ca06a0520dd2b71584896bf4ce1a395ba3
DhruvKaushal <dhruvkaushal1997@gmail.com>
2019-12-15 05:16:19 +0530

    resolved error in form post

--- couponSystem/profiles/views.py
+++ couponSystem/profiles/views.py
@@ -20,12 +20,16 @@ def adminKaPage(request):
 #     return render(request, 'profiles/userLogin.html', {'form': form})
 
 def userKaLog(request):
-    form = select_vendor_form()
-    if request.method=="POST" and form.is_valid():
-        choice = form.cleaned_data['Vendor_choices']
-        print(choice)
-        records = userData.objects.get(Vendor_number = ('choice')).aggregate(Sum('itemPrice'))
-        args = {'form': form, 'choice': choice, 'model':records}
-        return render(request, 'profiles/useLogin.html', {'args': args})
-    else:
-        return render(request, 'profiles/userLogin.html', {'form': form})
+  form = select_vendor_form()
+  # Check if the current method was a POST method
+  if request.method=="POST":
+    # Initiate the form again, but with post data
+    form = select_vendor_form(request.POST)
+    if form.is_valid():
+      choice = form.cleaned_data['Vendor_choices']
+      records = userData.objects.get(Vendor_number = ('choice')).aggregate(Sum('balance'))
+      args = {'form': form, 'choice': choice, 'model':records}
+      return render(request, 'profiles/useLogin.html', args)
+
+  # Return the normal results, form will display an error message if POST was triggered
+  return render(request, 'profiles/userLogin.html', {'form': form})
