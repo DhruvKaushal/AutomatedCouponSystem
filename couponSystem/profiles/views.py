5f4f74c0f26d36e18144545e33ef60de44c26021
DhruvKaushal <dhruvkaushal1997@gmail.com>
2020-01-19 22:30:13 +0530

    admin trans use done

--- couponSystem/profiles/views.py
+++ couponSystem/profiles/views.py
@@ -15,7 +15,19 @@ from .models import vendor, employee, transaction
 def adminKaPage(request):
     if request.method=="POST":
         if 'form1' in request.POST:
-            pass
+            vendor_choice = request.POST["inlineDefaultRadiosExample1"]
+            date_choice = request.POST["inlineDefaultRadiosExample1"]
+            x = employee.objects.get(name = request.user)
+            y = vendor.objects.get(id = vendor_choice)
+            if date_choice == 1:
+                d = transaction.objects.filter(ven_id=y, emp_id = x, timestamp__gte = datetime.date.today() - datetime.timedelta(days=30))
+            elif date_choice == 1:
+                d = transaction.objects.filter(ven_id=y, emp_id = x, timestamp__gte = datetime.date.today() - datetime.timedelta(days=60))
+            else:
+                d = transaction.objects.filter(vendor_id=y, emp_id = x, timestamp__gte = datetime.date.today() - datetime.timedelta(days=180))    
+
+            return render(request, 'profiles/adminKaLogin.html', {'model':d})        
+
 
         if 'form2' in request.POST:
             amount = request.POST["amt"]
